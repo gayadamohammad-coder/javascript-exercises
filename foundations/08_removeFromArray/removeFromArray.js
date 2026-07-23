34d5e5d38d5555a5b6c8d885b7b5ef8fbac37465
gayadamohammad-coder <gayadamohammad7@gmail.com>
2026-07-23 16:49:09 +0300

    feat: complete removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...removeArguments) {
+    let result = [];
+    for(let i =0;i<=arr.length-1;i++){
+        if (removeArguments.includes(arr[i])){
+              continue;
+             
+        }
+        result.push(arr[i]);
+    }
+
+    return result;
 };
 
 // Do not edit below this line
