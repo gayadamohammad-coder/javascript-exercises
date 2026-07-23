e8b39a799d6fde116d811705d2ec9a74c9928733
gayadamohammad-coder <gayadamohammad7@gmail.com>
2026-07-23 18:12:07 +0300

    feat: complete sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
+const sumAll = function(firstInt,secondInt) {
+    let sum=0;
+    if (!Number.isInteger(firstInt) || !Number.isInteger(secondInt) || firstInt < 0 ||
+    secondInt <0){
+         return "ERROR";
+    }
 
-};
+    let start = Math.min(firstInt,secondInt);
+    let end = Math.max(firstInt,secondInt);
+    
+    for (let i = start; i<=end;i++){
+        sum+=i;
+    }
+
+    return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
