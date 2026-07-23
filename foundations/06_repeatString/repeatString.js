79bebd0ac33bf467dd4b1de04608e2be9d14fd6c
gayadamohammad-coder <gayadamohammad7@gmail.com>
2026-07-23 07:16:31 +0300

    feat:complete 06_repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    let result ="";
+   if (num>0){
+    for (let i = 1;i<=num;i++){
+         result += string;
+    }
+   }else if (num===0){
+      return result;
+   }else{
+         return "ERROR";
+   }
+     
+   return result;
 };
 
 // Do not edit below this line
