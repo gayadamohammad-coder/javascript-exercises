e1106f20eafd50cd6cdda293c100cdd9f63963b6
gayadamohammad-coder <gayadamohammad7@gmail.com>
2026-07-23 07:43:25 +0300

    feat: complete reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let word="";
+       for (let i = text.length-1; i>=0 ;i--){
+        word +=text[i];
+            
+       }
+       return word;
 };
 
 // Do not edit below this line
