f380a379f28bb2ddaffc83802e6de39819ffef6f
gayadamohammad-coder <gayadamohammad7@gmail.com>
2026-09-08 20:24:07 +0300

    complete calculator exercise

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,26 +1,41 @@
-const add = function() {
-	
+const add = function(a,b) {
+	   return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	// first computes the sum of an empty array
+  if(array.length===0){
+    return 0;
+  }
+     return array.reduce((a,b)=> a+b );
+};
+  
+ 
+
+const multiply = function(array) {
+    return array.reduce((a,b)=> a*b);
 };
 
-const multiply = function() {
-
+const power = function(a,b) {
+	return a**b;
 };
 
-const power = function() {
-	
-};
+const factorial = function(n) {
+	if (n <= 1) {
+        
+        return 1;
+    }else{
+      return n * factorial(n - 1);
+    }
+    
+}
 
-const factorial = function() {
-	
-};
+
 
 // Do not edit below this line
 module.exports = {
