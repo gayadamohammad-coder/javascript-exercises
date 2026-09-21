0f2d0ffadd96e02d27cf4b8339b5ff4ca0edf424
gayadamohammad-coder <gayadamohammad7@gmail.com>
2026-09-21 07:39:58 +0300

    finnish objects palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
+/*so i need to make a function that takes a string and check if it reads from the back same as from the front so will turn
+this string to array and read it from front and read it from the back and compare if its equal or no 
+*/
+const palindromes = function (word) {
+   word =  word.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+   const arrayWord = word.split('');
+   const arrayRevWord = arrayWord.slice().reverse();
 
+   const result = arrayWord.every((letter, index) => {
+    return letter === arrayRevWord[index];
+});
+
+return result;
 };
+        
+
+  
+
 
 // Do not edit below this line
 module.exports = palindromes;
