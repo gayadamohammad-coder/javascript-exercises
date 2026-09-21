8beb459490e4d7c26a9e73f71b0a33c245fbc756
gayadamohammad-coder <gayadamohammad7@gmail.com>
2026-09-21 20:15:00 +0300

    finnish findTheOldest exercise

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+        let oldestPerson;
+        let oldestAge = 0;
+        let age;
+    for(const person of people){
+        
 
+    if (person.yearOfDeath !== undefined) {
+        age = person.yearOfDeath - person.yearOfBirth;
+    } else {
+        age = new Date().getFullYear() - person.yearOfBirth;
+    }
+
+    if(age>oldestAge){
+        oldestAge=age;
+        oldestPerson = person;
+    }
+
+    
+    }
+    return oldestPerson;
 };
 
 // Do not edit below this line
